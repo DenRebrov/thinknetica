18a201180ab38c7b188598472a8ffa632ecf4946
Den Rebrov <den_rebrov@list.ru>
2019-01-23 13:19:10 +0300

    Intermidiate correction

--- lesson2/2.rb
+++ lesson2/2.rb
@@ -1,4 +1,3 @@
-
 numbers = (10..100).step(5).to_a
 
 #puts numbers
--- lesson2/4.rb
+++ lesson2/4.rb
@@ -6,4 +6,4 @@ alphabet.each.with_index(1) do |letter, index|
   letters[letter] = index if vowels.include?(letter)
 end
 
-puts letters
+#puts letters
